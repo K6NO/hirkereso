a6004364ad372b115db82aed0fd658b016c44b3c
K6NO <tamas.kenessey@gmail.com>
2017-06-09 11:18:09 +0200

    set compression on server responses

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var app = express();
 
@@ -18,8 +19,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// static server
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression({level: 1}));
+
+//router
 app.use('/', index);
 
 // catch 404 and forward to error handler
